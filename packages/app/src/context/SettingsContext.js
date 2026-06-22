f7b8d33328a112099b981932935114cc931f0a53
jmawet <jenn.mawet@gmail.com>
2026-06-21 19:35:08 -0700

    flesh out recently released

--- packages/app/src/context/SettingsContext.js
+++ packages/app/src/context/SettingsContext.js
@@ -191,6 +191,7 @@ const LOCAL_TO_SERVER = Object.fromEntries(
 
 const TV_TO_SERVER_ROW = {
 	'latest-media': 'latestmedia',
+	'recently-released': 'recentlyreleased',
 	'library-tiles': 'smalllibrarytiles',
 	'favoriteMovies': 'favoritemovies',
 	'favoriteSeries': 'favoriteseries',
@@ -204,6 +205,7 @@ const TV_TO_SERVER_ROW = {
 };
 const SERVER_TO_TV_ROW = {
 	'latestmedia': 'latest-media',
+	'recentlyreleased': 'recently-released',
 	'smalllibrarytiles': 'library-tiles',
 	'favoritemovies': 'favoriteMovies',
 	'favoriteseries': 'favoriteSeries',
