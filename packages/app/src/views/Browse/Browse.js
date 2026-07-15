35ac544cbae0ea609bead01daf55fe550f38b72d
Matthew Sigal <matthewsigal@gmail.com>
2026-07-15 10:11:57 -0700

    fix: add catch fallback to getRandomItems API query on home page load
    This prevents slow database scans from timing out and crashing the entire home page loading sequence.

--- packages/app/src/views/Browse/Browse.js
+++ packages/app/src/views/Browse/Browse.js
@@ -940,7 +940,7 @@ const Browse = ({
 							api.getResumeItems(),
 							api.getNextUp(),
 							api.getUserConfiguration().catch(() => null),
-							api.getRandomItems(settings.featuredContentType, settings.featuredItemCount),
+							api.getRandomItems(settings.featuredContentType, settings.featuredItemCount).catch(() => null),
 							settings.mergeContinueWatchingNextUp ? api.getItems({
 								IncludeItemTypes: 'Episode',
 								Filters: 'IsPlayed',
